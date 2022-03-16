f7827272f30d350623a1ac523941f6591a7ec714
Katsuyuki Sampei <katsuyuki.sisyou@gmail.com>
2022-03-16 16:26:56 +0900

    プレビュー実装

---
+++ src/pages/blog/draft.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react"
+import { PageProps } from "gatsby"
+import queryString from "query-string"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const BlogPage = ({ location }) => {
+  const { contentId, draftKey } = queryString.parse(location.search);
+  const [data, setData] = useState({microcmsBlog: [] });
+
+  useEffect(() => {
+    fetch(`https://8w7mnb9ia6.microcms.io/api/v1/blog/${contentId}?draftKey=${draftKey}`, {
+      headers: {
+        'X-MICROCMS-API-KEY' : "810293baad604761a447483baa169272b2e2",
+      },
+    })
+      .then(res => res.json())
+      .then(res => setData({ microcmsBlog: res }));  // ※1
+  }, []);  // ※2
+
+  if (data === null) {
+    return null;
+  }
+  console.log(data);
+
+  return (
+    <Layout>
+        <SEO title={data.microcmsBlog.title} />
+        <span>{data.microcmsBlog.writer.name}</span>
+        <h1>{data.microcmsBlog.title}</h1>
+        <div
+        dangerouslySetInnerHTML={{
+            __html: `${data.microcmsBlog.body}`,
+        }}
+        />
+    </Layout>
+  )
+}
+
+export default BlogPage
